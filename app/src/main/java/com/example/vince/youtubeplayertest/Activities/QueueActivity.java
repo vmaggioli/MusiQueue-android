1c2cc9ac8503fccd9c725c63426e2f4b075b1225
Vincent Maggioli <vinmags16@gmail.com>
2017-01-21 06:54:39 -0500

    Gave up on RecyclerView.  Queue now works invisibly.

--- app/src/main/java/com/example/vince/youtubeplayertest/Activities/QueueActivity.java
+++ app/src/main/java/com/example/vince/youtubeplayertest/Activities/QueueActivity.java
@@ -1,27 +1,25 @@
-package com.example.vince.youtubeplayertest;
+package com.example.vince.youtubeplayertest.Activities;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
+import com.example.vince.youtubeplayertest.R;
 import com.google.android.youtube.player.YouTubeInitializationResult;
 import com.google.android.youtube.player.YouTubePlayer;
 import com.google.android.youtube.player.YouTubePlayerFragment;
 
+import java.util.LinkedList;
+
 public class QueueActivity extends AppCompatActivity {
     final public String API_KEY = "AIzaSyDtCJTBSLt9M1Xi_EBr49Uk4W8q4HhFHPU";
     private YouTubePlayer mYouTubePlayer;
+    LinkedList<String> queue = new LinkedList<>();
 
-    public class QueueItem {
-
-    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,12 +40,46 @@ public class QueueActivity extends AppCompatActivity {
             @Override
             public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
                 mYouTubePlayer = youTubePlayer;
+                mYouTubePlayer.setPlayerStateChangeListener(new YouTubePlayer.PlayerStateChangeListener() {
+                    @Override
+                    public void onLoading() {
+
+                    }
+
+                    @Override
+                    public void onLoaded(String s) {
+
+                    }
+
+                    @Override
+                    public void onAdStarted() {
+
+                    }
+
+                    @Override
+                    public void onVideoStarted() {
+
+                    }
+
+                    @Override
+                    public void onVideoEnded() {
+                        queue.removeFirst();
+                        if (queue.size() != 0)
+                            mYouTubePlayer.loadVideo(queue.peek());
+                    }
+
+                    @Override
+                    public void onError(YouTubePlayer.ErrorReason errorReason) {
+
+                    }
+                });
             }
         });
 
         url_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // retrieve video_id from url
                 String url = url_text.getText().toString();
                 String video_id = "";
                 int n = url.length();
@@ -57,7 +89,12 @@ public class QueueActivity extends AppCompatActivity {
                         break;
                     }
                 }
-                mYouTubePlayer.loadVideo(video_id);
+
+                // add new data to queue
+                if (queue.size() == 0)
+                    mYouTubePlayer.loadVideo(video_id);
+                queue.addLast(video_id);
+                url_text.getText().clear();
             }
         });
     }
