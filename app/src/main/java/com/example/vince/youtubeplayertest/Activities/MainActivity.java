bf0109de53ffb06af16347947904a9c8e82541e9
Vincent Maggioli <vinmags16@gmail.com>
2017-01-21 07:22:37 -0500

    Final commit before merge to master.

--- app/src/main/java/com/example/vince/youtubeplayertest/Activities/MainActivity.java
+++ app/src/main/java/com/example/vince/youtubeplayertest/Activities/MainActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import com.example.vince.youtubeplayertest.QueueActivity;
 import com.example.vince.youtubeplayertest.R;
 
 public class MainActivity extends AppCompatActivity {
