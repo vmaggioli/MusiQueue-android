20585f32e655d2846975f0a0c3ed705f51d50d4e
Vincent Maggioli <vinmags16@gmail.com>
2017-01-21 00:23:44 -0500

    Modified end product.  Video is now queued and played all in one activity.

--- app/src/main/java/com/example/vince/youtubeplayertest/QueueActivity.java
+++ app/src/main/java/com/example/vince/youtubeplayertest/QueueActivity.java
@@ -1,29 +1,64 @@
 package com.example.vince.youtubeplayertest;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerFragment;
 
 public class QueueActivity extends AppCompatActivity {
+    final public String API_KEY = "AIzaSyDtCJTBSLt9M1Xi_EBr49Uk4W8q4HhFHPU";
+    private YouTubePlayer mYouTubePlayer;
 
+    public class QueueItem {
+
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_queue);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        final EditText url_text = (EditText) findViewById(R.id.url);
+        Button url_button = (Button) findViewById(R.id.url_button);
+
+        // initialize YouTube player
+        YouTubePlayerFragment mYouTubePlayerFragment = (YouTubePlayerFragment)
+                getFragmentManager().findFragmentById(R.id.youtube_player);
+        mYouTubePlayerFragment.initialize(API_KEY, new YouTubePlayer.OnInitializedListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+
+            }
+
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+                mYouTubePlayer = youTubePlayer;
+            }
+        });
+
+        url_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = url_text.getText().toString();
+                String video_id = "";
+                int n = url.length();
+                for (int i = 0; i < n; i++) {
+                    if (url.substring(i, i + 1).equals("=")) {
+                        video_id = url.substring(i + 1, n);
+                        break;
+                    }
+                }
+                mYouTubePlayer.loadVideo(video_id);
             }
         });
     }
-
 }
