b6878593dc96768bd4a8731ca7829c671dc25afe
EC2 Default User <ec2-user@ip-10-0-0-91.us-east-2.compute.internal>
2017-02-22 18:46:55 +0000

    Added server stuff to repo.
    Some of the server's contents are now store within the repository. The server's file system is much larger than what was ported, but as of right now all we need is its www file to be backed up.

---
+++ MusiQueue-server/www/html/backendTest.php
@@ -0,0 +1,30 @@
+<?php include "../inc/dbinfo.inc"; ?>
+<?php
+  $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+  if (mysqli_connect_errno()) {
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    exit();
+  }
+
+  $name = isset($_POST['name']) ? $_POST['name'] : '';
+  $addr = isset($_POST['addr']) ? $_POST['addr'] : '';
+
+  $stmt0 = $conn->prepare("INSERT INTO Employees (Name, Address) VALUES (?,?)");
+  $stmt0->bind_param("ss", $name, $addr);
+  $stmt0->execute();
+
+  $stmt = "SELECT * FROM Employees;";
+  $result = mysqli_query($conn, $stmt) or die($conn->error);
+
+  if ($result) {
+    while ($row = mysqli_fetch_row($result)) {
+      echo $row[1];
+      echo "\t";
+      echo $row[2];
+      echo "\n";
+    }
+  } else {
+    echo "Failed to Login";
+  }
+  exit();
+?>
