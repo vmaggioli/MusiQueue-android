b18787a13847c7b955d24228472b0cca6b489984
bhanford <bhanford@purdue.edu>
2017-02-25 15:46:37 -0500

    synced up front end and back end of hubSearch
    The results of the search are correctly displaying in a scrollable screen
    on the front end. None of the elements are currently clickable and so we
    need to set it up so that they are clickable and upon being clicked, the
    user is correctly redirected to the corresponding 'joinHub' page.

--- MusiQueue-android/app/src/main/java/com/example/vince/youtubeplayertest/Activities/users_only/SearchHub.java
+++ MusiQueue-android/app/src/main/java/com/example/vince/youtubeplayertest/Activities/users_only/SearchHub.java
@@ -19,12 +19,15 @@ import com.example.vince.youtubeplayertest.Activities.helper_classes.HubsListIte
 import com.example.vince.youtubeplayertest.R;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class SearchHub extends AppCompatActivity {
     RecyclerView hubsList;
     EditText enterHub;
     Button searchButton;
     ArrayList<HubsListItem> hubs;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,12 +37,6 @@ public class SearchHub extends AppCompatActivity {
         // initialize views
         enterHub = (EditText) findViewById(R.id.hub_name_search);
         searchButton = (Button) findViewById(R.id.hub_name_search_button);
-//        searchButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//
-//            }
-//        });
 
         // TODO: find out how to add the data from AWS
         hubsList = (RecyclerView) findViewById(R.id.hubs_list);
@@ -58,13 +55,20 @@ public class SearchHub extends AppCompatActivity {
         if (enterHub.length() != 0) {
             String contents = enterHub.getText().toString();
 
-            BackgroundWorker backgroundWorker = new BackgroundWorker(this);
+            BackgroundWorker backgroundWorker = new BackgroundWorker(new BackgroundWorker.AsyncResponse() {
+                @Override
+                public void processFinish(String result) {
+                    List<String> nameList = new ArrayList<String>(Arrays.asList(result.split(", ")));
+                    hubs = new ArrayList<>();
+                    for (String name : nameList) {
+                        hubs.add(new HubsListItem(name));
+                    }
+                    HubsListAdapter hubsListAdapter = new HubsListAdapter(getApplicationContext(), hubs);
+                    hubsList.setAdapter(hubsListAdapter);
+                    hubsList.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+                }
+            });
             backgroundWorker.execute("searchHub", contents);
-
-            HubsListAdapter hubsListAdapter = new HubsListAdapter(this, hubs);
-            Log.d("poop", contents);
-            hubsList.setAdapter(hubsListAdapter);
-            hubsList.setLayoutManager(new LinearLayoutManager(this));
         }
     }
 }
