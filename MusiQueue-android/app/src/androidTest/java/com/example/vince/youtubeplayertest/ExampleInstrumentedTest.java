93a47359d6fd3ec3f94d298e5f7d22e4aa8786f3
bhanford <bhanford@purdue.edu>
2017-03-20 02:50:53 -0400

    Automated UI testing is now possible
    Some, possibly all, dependencies have been added for automated UI testing.
    We will be using "espresso" which makes automated testing trivial and
    allows developers to jump to any activity and test its functionality
    instead of having to manually navigate to the activity you want to test.
    An example test-case has been written up within the ExampleInstrumentedTest
    class under the androidTest directory. The test-case types a string into
    the hub search, presses the search button and finally selects an item
    within the recycler view. We should create as many test cases for each
    activity as we can and keep them short and simple so that they are
    thoroughly testing individual parts of the app.

--- MusiQueue-android/app/src/androidTest/java/com/example/vince/youtubeplayertest/ExampleInstrumentedTest.java
+++ MusiQueue-android/app/src/androidTest/java/com/example/vince/youtubeplayertest/ExampleInstrumentedTest.java
@@ -1,13 +1,20 @@
 package com.example.vince.youtubeplayertest;
 
-import android.content.Context;
-import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.contrib.RecyclerViewActions;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import com.example.vince.youtubeplayertest.Activities.users_only.SearchHub;
+
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 
 /**
  * Instrumentation test, which will execute on an Android device.
@@ -16,11 +23,22 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
-    @Test
-    public void useAppContext() throws Exception {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.vince.youtubeplayertest", appContext.getPackageName());
+    // the rule allows you to jump directly to a specific activity
+    @Rule
+    public ActivityTestRule<SearchHub> searchHubActivityTestRule = new ActivityTestRule<SearchHub>(SearchHub.class);
+
+    @Test
+    public void typeAcceptableSearch() throws Exception {
+
+        onView(withId(R.id.hub_name_search))                    // choose view to manipulate (search box)
+                .perform(typeText("hub"), closeSoftKeyboard()); // type "hub" into search bar and close keyboard
+
+        onView(withId(R.id.hub_name_search_button))             // choose view to manipulate (search button)
+                .perform(click());                              // click on search button
+
+        onView(withId(R.id.hubs_list))                          // choose view to manipulate (recycler view)
+                .perform(RecyclerViewActions.actionOnItemAtPosition(3, click()));   // click third item on list
     }
+
 }
