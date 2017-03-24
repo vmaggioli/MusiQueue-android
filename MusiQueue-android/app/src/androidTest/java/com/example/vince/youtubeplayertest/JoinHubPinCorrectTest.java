d3df744a0a5ded4b45c967ecd107e8ab41444e67
bhanford <bhanford@purdue.edu>
2017-03-24 02:40:03 -0400

    Added more automated UI testcases
    Upon using the automated UI testcases I was able to find a few more places
    that were either performing incorrectly or could have been performing
    better. I made some cosmetic changes for easier use of the app.
    
    One test case seems to fail because a video tries to load when a brand new
    hub is created (we should fix this if it is possible to do so). Another
    test case is failing because we do not yet have a check implemented
    checking that the user trying to join a hub types the correct pin number
    (I went ahead and added a check for making sure that exactly 4 digits were
    inserted into the hubpin edit text box).

---
+++ MusiQueue-android/app/src/androidTest/java/com/example/vince/youtubeplayertest/JoinHubPinCorrectTest.java
@@ -0,0 +1,47 @@
+package com.example.vince.youtubeplayertest;
+
+import android.support.test.espresso.contrib.RecyclerViewActions;
+import android.support.test.espresso.intent.rule.IntentsTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import com.example.vince.youtubeplayertest.Activities.users_only.SearchHub;
+import com.example.vince.youtubeplayertest.Activities.users_only.ViewQueueActivity;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+/**
+ * Created by Brian on 3/24/2017.
+ */
+
+@RunWith(AndroidJUnit4.class)
+
+public class JoinHubPinCorrectTest {
+    @Rule
+    public IntentsTestRule<SearchHub> checkCorrectJoin = new IntentsTestRule<SearchHub>(SearchHub.class);
+
+    @Test
+    public void correctJoinTest() throws Exception {
+        onView(withId(R.id.hub_name_search))
+                .perform(typeText("test"), closeSoftKeyboard());
+        onView(withId(R.id.hub_name_search_button))
+                .perform(click());
+        onView(withId(R.id.hubs_list))
+                .perform(RecyclerViewActions.actionOnItemAtPosition(2,click()));
+        onView(withId(R.id.pass_pin))
+                .perform(typeText("1234"), closeSoftKeyboard());
+        onView(withId(R.id.join_hub_button))
+                .perform(click());
+        intended(hasComponent(ViewQueueActivity.class.getName()));
+    }
+
+}
