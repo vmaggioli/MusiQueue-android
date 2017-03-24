8052e01b2337fc0d1bb9b1d60a1b79a2ccf11f8c
vgalla <sairamgalla60@gmail.com>
2017-03-24 13:21:08 -0400

    updated ViewQueueActivity to show the list to a hub user that is not the hub owner.

--- MusiQueue-android/app/src/main/java/com/example/vince/youtubeplayertest/Activities/users_only/ViewQueueActivity.java
+++ MusiQueue-android/app/src/main/java/com/example/vince/youtubeplayertest/Activities/users_only/ViewQueueActivity.java
@@ -3,15 +3,21 @@ package com.example.vince.youtubeplayertest.Activities.users_only;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.widget.TextView;
 
+import com.example.vince.youtubeplayertest.Activities.VideoItemAdapter;
 import com.example.vince.youtubeplayertest.Activities.helper_classes.Hub;
+import com.example.vince.youtubeplayertest.Activities.helper_classes.HubSingleton;
+import com.example.vince.youtubeplayertest.Activities.hub_admin_only.QueueActivity;
 import com.example.vince.youtubeplayertest.R;
 
 public class ViewQueueActivity extends AppCompatActivity {
     TextView hubNameView;
 
     Hub appState;
+    RecyclerView songListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +30,22 @@ public class ViewQueueActivity extends AppCompatActivity {
         hubNameView.setText(appState.getHubName());
 
         // TODO: show the queue or whatever
+        HubSingleton hubSingleton = HubSingleton.getInstance();                         // SINGLETON HERE
+
+        VideoItemAdapter adapter = new VideoItemAdapter(ViewQueueActivity.this, hubSingleton.getEntireList(), new VideoItemAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(QueueSong videoItem) {
+
+            }
+        });
+        songListView = (RecyclerView) findViewById(R.id.song_List);
+        songListView.setAdapter(adapter);
+        songListView.setLayoutManager(new LinearLayoutManager(this));
+
+        QueueSong song = new QueueSong();
+        song.setTitle("hello");
+        hubSingleton.add(song);
+
+        //implement a search button
     }
 }
