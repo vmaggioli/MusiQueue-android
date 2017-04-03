54c60463312358a0c6fe764a459041e9bdec9389
John Stimac <jcubed111@gmail.com>
2017-04-03 17:13:19 -0400

    Adds smart vote counting to the backend.

--- MusiQueue-server/www/html/api/voteDownSong.php
+++ MusiQueue-server/www/html/api/voteDownSong.php
@@ -1,32 +1,6 @@
 <?php
-require_once "connection.php";
 
-apiDocs("
-voteDownSong:
-	Params:
-		hubId - the hub the song is on
-		phoneId - the phone id of the user voting
-		songId - the id of the song entry
-	Returns on success:
-		See hubSongList endpoint.
-");
-apiDocsCouldError("DB_ISSUE");
-
-require_once "assert/hubExists.php";
-require_once "assert/userConnectedToHub.php";
-require_once "assert/hubIdOpen.php";
-require_once "assert/songOnHub.php";
-
-$result = mysqli_query($conn, "
-	UPDATE Songs
-	SET down_votes = down_votes + 1
-	WHERE id = '$songId'
-");
-
-if(!$result) {
-	respondError("DB_ISSUE", "Could not decrease vote total on song");
-}
-
-require "hubSongList.php";
+define("VOTE_DIRECTION", -1);
+require "util/voteSong.php";
 
 ?>
