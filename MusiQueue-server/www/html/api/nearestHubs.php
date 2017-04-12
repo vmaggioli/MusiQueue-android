2f883e3452b18fceeb73ac0c6634e611c5010bfe
bhanford <bhanford@purdue.edu>
2017-04-12 00:45:56 -0400

    forgot to transfer php, sorry

---
+++ MusiQueue-server/www/html/api/nearestHubs.php
@@ -0,0 +1,45 @@
+<?php
+require_once "connection.php";
+
+apiDocs("
+recentHubs:
+	Params:
+		  phoneId - the user's phoneId
+	Returns on success:
+		  array of hub names pertaining to the hubs the user has been active in
+      within the past 7 days
+");
+
+$phoneId = mysqli_real_escape_string($conn, $_REQUEST['phoneId']);
+$lat = mysqli_real_escape_string($conn, $_REQUEST['lat']);
+$long = mysqli_real_escape_string($conn, $_REQUEST['long']);
+
+$minLat = $lat - 0.01;
+$maxLat = $lat + 0.01;
+$minLong = $long - 0.01;
+$maxLong = $long + 0.01;
+
+$result = mysqli_query($conn, "
+	SELECT Hubs.hub_name
+  FROM Hubs
+  INNER JOIN Users
+  ON Users.hub_id = Hubs.id
+  WHERE Hubs.latitude BETWEEN '$minLat' AND '$maxLat'
+  AND Hubs.longitude BETWEEN '$minLong' AND '$maxLong'
+");
+
+
+$arr = array();
+
+if ($result->num_rows == 0) {
+    respondSuccess($arr);
+}
+
+while($assoc = mysqli_fetch_assoc($result)) {
+	//$assoc['hub_pin_required'] = !!$assoc['hub_pin_required'];
+	$arr[] = $assoc;
+}
+
+respondSuccess($arr);
+
+?>
