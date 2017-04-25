88a759b7499a5602b49210905b7bc3fb2f324779
John Stimac <jcubed111@gmail.com>
2017-04-25 13:04:38 -0400

    Adds icon to web.

--- MusiQueue-server/www/html/index.php
+++ MusiQueue-server/www/html/index.php
@@ -1,3 +1,4 @@
+<link rel="shortcut icon" href="favicon.ico" />
 You might be looking for: <a href='api'>api docs</a>.<br/>
 Or maybe <a href="testApi.html">api tests</a>.<br/>
 Or maybe <a href="bruteForceTest.html">brute force tests</a>.<br/>
